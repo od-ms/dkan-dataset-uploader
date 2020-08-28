0bd287eef00809199e39081dca79e79839c4e8db
Thomas Werner <thomas.w.werner@gmail.com>
2020-08-28 17:35:20 +0200

    Initial test version with a basic download mode

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # dkan-dataset-uploader
-Gui client to upload or download multiple datasets and resources from or to a dkan instance via excel sheets
+A tool to upload or download multiple datasets and resources from or to a dkan instance via excel sheets
+
+
+## Usage
+
+```bash
+    # Start client in interactive GUI window mode
+    python3 -m dkan-uploader
+
+    # Start client in console mode
+    python3 -m dkan-uploader filename.xlsx [--download]
+```
+
+## Python learning resources
+* **Python cheat sheet** https://www.pythoncheatsheet.org/
+* **Python example project setup** https://github.com/navdeep-G/samplemod
+* **Github actions for python**: https://docs.github.com/en/actions/language-and-framework-guides/using-python-with-github-actions
+* 
---
+++ dkan-uploader/app.py
@@ -0,0 +1,43 @@
+"""Main source file for DKAN REMOTE CONTROL
+"""
+
+import argparse
+import textwrap
+import logging
+
+from . import excelreader
+
+logging.basicConfig(level=logging.DEBUG, format='<%(asctime)s %(levelname)s> %(message)s')
+
+class DkanUploader:
+    """Main file DKAN REMOTE CONTROL"""
+
+    @staticmethod
+    def run():
+        """ERROR: Too many docstrings required"""
+
+        logging.info('Start of program')
+
+        # Print usage instructions
+        parser = argparse.ArgumentParser(
+            prog='dkan-uploader',
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            epilog=textwrap.dedent('''\
+        DKAN remote control - instructions
+        ==================================
+
+        Run this file without any command line arguments to use the interactive GUI window mode.
+
+        If you want want to automate the up- or download of data to a DKAN instance, then use the command line switches as described above.
+        '''))
+
+        # Parse command line arguments
+        parser.add_argument('filename', type=str, nargs='?', help='Filename of excel file with data that should be transfered to the DKAN instance')
+        parser.add_argument('-d', '--download', action='store_true',
+            help='Run in DOWNLOAD mode: The excel file will be overwritten! It will be filled with data from the DKAN instance')
+
+        args = parser.parse_args()
+        logging.info("Command line arguments: %s", args)
+
+        if args.download:
+            excelreader.read()
