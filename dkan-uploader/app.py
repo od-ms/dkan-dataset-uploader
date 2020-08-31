2f776726a5a19fed4ec8a5bb04ec2ab70c2440eb
Thomas Werner <opendata@citeq.de>
2020-08-31 12:35:51 +0200

    Start GUI from main file

--- dkan-uploader/app.py
+++ dkan-uploader/app.py
@@ -6,6 +6,7 @@ import textwrap
 import logging
 
 from . import excelreader
+from . import main_gui
 
 logging.basicConfig(level=logging.DEBUG, format='<%(asctime)s %(levelname)s> %(message)s')
 
@@ -34,10 +35,12 @@ class DkanUploader:
         # Parse command line arguments
         parser.add_argument('filename', type=str, nargs='?', help='Filename of excel file with data that should be transfered to the DKAN instance')
         parser.add_argument('-d', '--download', action='store_true',
-            help='Run in DOWNLOAD mode: The excel file will be overwritten! It will be filled with data from the DKAN instance')
+                            help='Run in DOWNLOAD mode: The excel file will be overwritten! It will be filled with data from the DKAN instance')
 
         args = parser.parse_args()
         logging.info("Command line arguments: %s", args)
 
         if args.download:
             excelreader.read()
+        else:
+            main_gui.show()
--- dkan-uploader/main_gui.py
+++ dkan-uploader/main_gui.py
@@ -1,11 +1,11 @@
 import random
-from tkinter import Tk, Label, Button, Entry, StringVar, DISABLED, NORMAL, END, N, S, W, E
+from tkinter import Tk, Label, Button, Entry, StringVar, Text, DISABLED, INSERT, NORMAL, END, N, S, W, E
 from tkinter import ttk
 
 
-class GuessingGame:
+class MainGui:
     def __init__(self, window):
-        master = ttk.Frame(window, padding=(3,3,12,12))
+        master = ttk.Frame(window, padding=(20,30,12,12))
         master.grid(column=0, row=0, sticky=(N, S, E, W))
 
         self.master = master
@@ -13,40 +13,64 @@ class GuessingGame:
 
         # -- Main layout --
 
+        currentRow = 0
+        y_spacing = 10
+
+        print("row" )
+
         # Headline
         headline_label = Label(master, text="DKAN Uploader", font=("Arial Bold", 30))
         headline_label.grid(row=0, columnspan=3)
 
-        # Url input
+        # Filename input
+        currentRow += 1
         vcmd = master.register(self.validate) # we have to wrap the command
+        self.filename_input = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
+        self.filename_input.grid(row=currentRow, column=1, columnspan=2, sticky=W+E, pady=(y_spacing, y_spacing))
+        self.filename_label = Label(master, text="Excel-Filename:")
+        self.filename_label.grid(row=currentRow, column=0, sticky=E, pady=(y_spacing, y_spacing))
+
+
+        currentRow += 1
         self.url_input = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
-        self.url_input.grid(row=1, column=1, columnspan=2, sticky=W+E)
+        self.url_input.grid(row=currentRow, column=1, columnspan=2, sticky=W+E)
         self.url_label = Label(master, text="DKAN-Url:")
-        self.url_label.grid(row=1, column=0, sticky=W+E)
+        self.url_label.grid(row=currentRow, column=0, sticky=E)
 
-        # #filename input
-        vcmd = master.register(self.validate) # we have to wrap the command
-        self.url_input = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
-        self.url_input.grid(row=1, column=1, columnspan=2, sticky=W+E)
-        self.url_label = Label(master, text="DKAN-Url:")
-        self.url_label.grid(row=1, column=0, sticky=W+E)
-
-        #credentials
-
-        # logoutput
-
-        
-        # DKAN 
-
-        # Log File field
+        # cDKAN redentials
+        currentRow += 1
+        self.user_input = Entry(master)
+        self.user_input.grid(row=currentRow, column=1,  sticky=W+E)
+        self.user_input = Entry(master, show="*")
+        self.user_input.grid(row=currentRow, column=2, sticky=W+E)
+        self.cred_label = Label(master, text="User/Password:")
+        self.cred_label.grid(row=currentRow, column=0, sticky=E)
 
         # Last row (Buttons)
+        currentRow += 1
+
         aktion_label = Label(master, text="Aktion:")
-        aktion_label.grid(row=2, column=0)
+        aktion_label.grid(row=currentRow, column=0, sticky=E, pady=(y_spacing, 0))
         self.upload_button = Button(master, text="Excel -> DKAN", command=self.action_upload)
-        self.upload_button.grid(row=2, column=1, sticky=W+E)
+        self.upload_button.grid(row=currentRow, column=1, sticky=W+E, pady=(y_spacing, 0))
         self.download_button = Button(master, text="DKAN -> Excel", command=self.action_download, state=DISABLED)
-        self.download_button.grid(row=2, column=2, sticky=W+E)
+        self.download_button.grid(row=currentRow, column=2, sticky=W+E, pady=(y_spacing, 0))
+
+        # Log File field
+        master_right = ttk.Frame(window, padding=(3, 3, 12, 12))
+        master_right.grid(column=1, row=0, sticky=(N, S, E, W))
+        self.info_box = Text(master_right)
+        self.info_box.insert(INSERT, "Logfenster mit Logausgaben\n")
+        self.info_box.insert(INSERT, "==========================\n\n")
+        self.info_box.insert(END, "Hello!\n")
+        self.info_box.config(state=DISABLED)
+        self.info_box.grid(row=0, column=0, sticky=(N, S, E, W))
+
+        # Add something to log field
+        self.info_box.config(state=NORMAL)
+        #self.info_box.delete(1.0, END)
+        self.info_box.insert(END, "Bye bye! ..... ... \n")
+        self.info_box.config(state=DISABLED)
 
 
 
@@ -96,6 +120,7 @@ class GuessingGame:
         self.download_button.configure(state=NORMAL)
         self.upload_button.configure(state=DISABLED)
 
-root = Tk()
-my_gui = GuessingGame(root)
-root.mainloop()
+def show():
+    root = Tk()
+    my_gui = MainGui(root)
+    root.mainloop()
