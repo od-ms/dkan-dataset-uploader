52d06395163befd04168489edbb4db4215d5cd6d
Thomas Werner <thomas.w.werner@gmail.com>
2020-12-16 09:31:59 +0100

    add command line switch for upload feature

--- dkan-uploader/app.py
+++ dkan-uploader/app.py
@@ -4,8 +4,8 @@
 import argparse
 import textwrap
 import logging
-import configparser
 from . import excelwriter
+from . import excelreader
 from . import main_gui
 from . import config
 from . import confighandler
@@ -30,6 +30,8 @@ class DkanUploader:
 
         if args.download:
             excelwriter.write(args.filename)
+        elif args.upload:
+            excelreader.read(args.filename)
         else:
             print("")
             print("Starting in GUI mode. To print available command line options, start with --help.")
@@ -43,24 +45,29 @@ class DkanUploader:
         # Print usage instructions
         parser = argparse.ArgumentParser(
             prog='dkan-uploader',
-            formatter_class=argparse.RawDescriptionHelpFormatter,
+            formatter_class=argparse.RawTextHelpFormatter,
             epilog=textwrap.dedent('''\
         DKAN remote control - instructions
         ==================================
 
-        Run this file without any command line arguments to use the interactive GUI window mode.
-
+        Running this without any command line arguments will start the interactive GUI window mode.
         If you want want to automate the up- or download of data to a DKAN instance, then use the command line switches as described above.
         '''))
 
-        # Parse command line arguments
+        # Command line arguments for DOWNLOAD
         parser.add_argument('filename', type=str, nargs='?', help='Filename of excel file with data that should be transfered to the DKAN instance')
+        parser.add_argument('-n', '--ids', action='store', dest='ids',
+            help='Comma separated list of dataset IDs. Limits action to these dataset IDs (upload or download)\n\n')
+
+
         parser.add_argument('-d', '--download', action='store_true',
             help='Run in DOWNLOAD mode: The excel file will be overwritten! It will be filled with data from the DKAN instance')
-        parser.add_argument('-or', '--overwrite-rows', action='store_true', dest='overwrite',
-            help='If datasets already exist in the excel file, overwrite the row content. (Default: no)')
-        parser.add_argument('-n', '--ids', action='store', dest='ids',
-            help='Comma separated list of dataset IDs. Limits action to these dataset IDs (upload or download)')
+        parser.add_argument('-or', '--overwrite', action='store_true', dest='overwrite',
+            help='If datasets already exist in the excel file, overwrite the row content. (Default: no)\n\n')
+
+        # Command line arguments for UPLOAD
+        parser.add_argument('-u', '--upload', action='store_true',
+            help='Run in UPLOAD mode: The DKAN content will be overwritten with data from the excel file')
 
         args = parser.parse_args()
         logging.info("Command line arguments: %s", args)
