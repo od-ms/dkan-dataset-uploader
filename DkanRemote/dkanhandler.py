c70b0f7045cbd2d04ee213f4be5373cb485f6bcf
Thomas Werner <opendata@citeq.de>
2023-03-03 11:48:03 +0100

    disable ssl verify .. Doesn't work

--- DkanRemote/dkanhandler.py
+++ DkanRemote/dkanhandler.py
@@ -10,6 +10,46 @@ from . import config
 
 # pylint: disable=global-statement
 
+import requests
+import warnings
+import contextlib
+from urllib3.exceptions import InsecureRequestWarning
+
+
+old_merge_environment_settings = requests.Session.merge_environment_settings
+
+# Fix problems with broken SSL certificates
+# ref. https://stackoverflow.com/questions/15445981/how-do-i-disable-the-security-certificate-check-in-python-requests
+@contextlib.contextmanager
+def no_ssl_verification():
+    opened_adapters = set()
+
+    def merge_environment_settings(self, url, proxies, stream, verify, cert):
+        # Verification happens only once per connection so we need to close
+        # all the opened adapters once we're done. Otherwise, the effects of
+        # verify=False persist beyond the end of this context manager.
+        opened_adapters.add(self.get_adapter(url))
+
+        settings = old_merge_environment_settings(self, url, proxies, stream, verify, cert)
+        settings['verify'] = False
+
+        return settings
+
+    requests.Session.merge_environment_settings = merge_environment_settings
+
+    try:
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', InsecureRequestWarning)
+            yield
+    finally:
+        requests.Session.merge_environment_settings = old_merge_environment_settings
+
+        for adapter in opened_adapters:
+            try:
+                adapter.close()
+            except:
+                pass
+
 api = None
 
 def expand_into(varname, id_list):
@@ -188,7 +228,8 @@ def connect():
     try:
         logging.debug(_("DKAN-Login: %s @ %s"), config.dkan_username, config.dkan_url)
         # Last parameter is debug mode: True = Debugging ON
-        api = DatasetAPI(config.dkan_url, config.dkan_username, config.dkan_password, True)
+        with no_ssl_verification():
+            api = DatasetAPI(config.dkan_url, config.dkan_username, config.dkan_password, True)
         return ""
     except LoginError as err:
         logging.error(_("Fehler bei Verbindung zur DKAN-Instanz!"))
